6ecf5980f02d9a4a9d16d1432c7d01bed8a75140
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-12-05 18:33:58 +0100

    Use clap

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,60 @@
+use clap::{Arg, Command};
 use reqwest::blocking::Client;
 use scraper::{Html, Selector};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Get the ISIN from the user
-    println!("Enter the ISIN:");
-    let mut isin = String::new();
-    std::io::stdin().read_line(&mut isin)?;
-    let isin = isin.trim();
+    // Define the command-line arguments
+    let matches = Command::new("Euronext Data Extractor")
+        .version("1.0")
+        .author("Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>")
+        .about("Extracts financial data from the Euronext website")
+        .arg(
+            Arg::new("isin")
+                .short('i')
+                .long("isin")
+                .value_name("ISIN")
+                .help("The ISIN of the instrument to fetch data for")
+                .num_args(1)
+                .required(true),
+        )
+        .arg(
+            Arg::new("format")
+                .short('f')
+                .long("format")
+                .value_name("FORMAT")
+                .help("Output format: price, abs, rel")
+                .num_args(1)
+                .required(true)
+                .value_parser(["price", "abs", "rel"]),
+        )
+        .get_matches();
+
+    // Get the ISIN and format from arguments
+    let isin = matches.get_one::<String>("isin").unwrap();
+    let format = matches.get_one::<String>("format").unwrap();
 
     // Construct the URL
-    let url = format!("https://live.euronext.com/en/ajax/getDetailedQuote/{}-XAMS", isin);
+    let url = format!(
+        "https://live.euronext.com/en/ajax/getDetailedQuote/{}-XAMS",
+        isin
+    );
 
     // Fetch the HTML from the URL
     let client = Client::new();
     let response = client.get(&url).send()?.text()?;
-    
+
     // Parse the HTML
     let document = Html::parse_document(&response);
 
-    // Extract the required data
+    // Define CSS selectors
     let price_selector = Selector::parse("#header-instrument-price").unwrap();
-    let since_open_selector = Selector::parse("div + span").unwrap();
-    let since_open_percent_selector = Selector::parse("div + span + span").unwrap();
-    let since_previous_selector = Selector::parse("div + span").unwrap();
-    let since_previous_percent_selector = Selector::parse("div + span + span").unwrap();
+    let since_open_selector = Selector::parse(".data-header__col-right .col:nth-of-type(1) span.data-24").unwrap();
+    let since_open_percent_selector = Selector::parse(".data-header__col-right .col:nth-of-type(1) span.text-ui-grey-1").unwrap();
+    let since_previous_selector = Selector::parse(".data-header__col-right .col:nth-of-type(2) span.data-24").unwrap();
+    let since_previous_percent_selector = Selector::parse(".data-header__col-right .col:nth-of-type(2) span.text-ui-grey-1").unwrap();
 
-    // Extract the data
+    // Extract data
     let price = document
         .select(&price_selector)
         .next()
@@ -57,10 +85,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         .and_then(|el| el.text().next())
         .unwrap_or("N/A");
 
-    // Print the results
-    println!("Price: {}", price);
-    println!("Since Open: {} ({})", since_open, since_open_percent);
-    println!("Since Previous Close: {} ({})", since_previous, since_previous_percent);
+    // Format the output
+    match format.as_str() {
+        "price" => println!("Price: {}", price),
+        "abs" => println!(
+            "Price: {}, Since Open: {} ({}), Since Previous Close: {} ({})",
+            price, since_open, since_open_percent, since_previous, since_previous_percent
+        ),
+        "rel" => println!(
+            "Price: {}, Since Open: {}, Since Previous Close: {}",
+            price, since_open_percent, since_previous_percent
+        ),
+        _ => unreachable!("Invalid format"),
+    }
 
     Ok(())
 }
